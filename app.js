ef002fe0ace36a7c1880e5d5df2670d6faff2040
prudhvi suchendra <suchendraprudhvi17@gmail.com>
2022-05-20 16:53:16 +0530

    added table users

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ connection.connect(function(err) {
   }
 
   console.log('Connected to database.');
-  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+  var sql = "CREATE TABLE users (name VARCHAR(255), address VARCHAR(255))";
   connection.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
