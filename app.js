e602d08647ca4237b9fdd903f5f6ada84f550d7b
prudhvi suchendra <suchendraprudhvi17@gmail.com>
2022-05-20 16:31:05 +0530

    ignored add table

--- app.js
+++ app.js
@@ -2,27 +2,27 @@ const express = require('express');
 const app = express();
 var mysql = require('mysql');
 
-// var connection = mysql.createConnection({
-//   host     : process.env.RDS_HOSTNAME,
-//   user     : process.env.RDS_USERNAME,
-//   password : process.env.RDS_PASSWORD,
-//   port     : process.env.RDS_PORT,
-//   database : "mydb"
-// });
+var connection = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT,
+  database : "mydb"
+});
 
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error('Database connection failed: ' + err.stack);
-//     return;
-//   }
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
 
-//   console.log('Connected to database.');
+  console.log('Connected to database.');
 //   var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
 //   con.query(sql, function (err, result) {
 //     if (err) throw err;
 //     console.log("Table created");
 //   });
-// });
+});
 
 app.get('/', (req, res) => {
     res.send("Updated");
