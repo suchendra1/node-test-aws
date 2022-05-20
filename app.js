2270e22f7d0243432c6a9c3fbadb07368a98b66b
prudhvi suchendra <suchendraprudhvi17@gmail.com>
2022-05-20 15:15:34 +0530

    added database

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const app = express();
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
 
 app.get('/', (req, res) => {
     res.send("Updated");
@@ -9,3 +27,5 @@ const port = process.env.port || 3010;
 app.listen(port, () => {
     console.log("Wazzappppp");
 });
+
+connection.end();
